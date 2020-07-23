f1cd15db4336a019f4ea4f73355753b83e89504e
backss3 <67251822+backss3@users.noreply.github.com>
2020-07-23 18:39:16 +0900

    table create

--- db/migrate/20200723082851_devise_create_users.rb
+++ db/migrate/20200723082851_devise_create_users.rb
@@ -4,9 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
+
+      t.string :nickname,           null: false 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false
+      t.string :family_name,        null: false
+      t.string :first_name_kana,    null: false
+      t.string :family_name_kana,   null: false
+      t.integer :birthday,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -37,10 +43,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :name, unique: true
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
+
