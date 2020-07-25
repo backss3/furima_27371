415f5ce0ccbe93943bfc8368a9de716321b3674a
backss3 <67251822+backss3@users.noreply.github.com>
2020-07-25 13:30:36 +0900

    sign-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_122846) do
+ActiveRecord::Schema.define(version: 2020_07_24_192233) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "image_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.bigint "category_id_id", null: false
+    t.bigint "status_id", null: false
+    t.integer "cost", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "day", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id_id"], name: "index_items_on_category_id_id"
+    t.index ["image_id"], name: "index_items_on_image_id"
+    t.index ["status_id"], name: "index_items_on_status_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
