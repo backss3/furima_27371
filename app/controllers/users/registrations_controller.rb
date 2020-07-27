0ef6ed5cd72ab0ffb0832e5bbaa91e7cfd7b64d4
backss3 <67251822+backss3@users.noreply.github.com>
2020-07-27 15:53:59 +0900

    出品するへのlink

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,6 +20,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   private
+
   def create_params
     params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :encrypted_password, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday)
   end
