9d03f707cc76f47607e819397af0beb0cd82d6cd
backss3 <67251822+backss3@users.noreply.github.com>
2020-08-01 17:23:10 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,48 +6,35 @@ describe Item do
     before do
       @item = build(:item)
     end
+      
     it "全て入力されていればOK" do
-      item = @item
+      item = create(:item)
       expect(item).to be_valid
     end
-
-    before do
-      @item = build(:item, name: nil)
-    end
     it "nameが空ならNG" do
-      item = @item
-      item.valid?
-      expect(iten.errors[:name]).to include("can't be blank")
-    end
-
-    before do
-      @item = build(:item, description: nil)
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors[:name]).to include("can't be blank")
     end
     it "explainが空ならNG" do
-      item = @item
-      item.valid?
-      expect(item.errors[:description]).to include("can't be blank")
-    end
-
-    before do
-      @item = build(:item, price: nil)
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors[:description]).to include("can't be blank")
     end
     it "priceが空ならNG" do
-      item = @item
-      item.valid?
-      expect(product.errors[:price]).to include("can't be blank")
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors[:price]).to include("can't be blank")
     end
-
     it "priceが300円未満ならNG" do
-      item = build(:item, price: "290")
-      item.valid?
-      expect(item.errors[:price]).to include("は300以上の値にしてください")
+      @item.price = "290"
+      @item.valid?
+      expect(@item.errors[:price]).to include("は300以上の値にしてください")
     end
-    
     it "priceが9999999円よりも高額ならNG" do
-      item = build(:item, price: "19999999")
-      item.valid?
-      expect(item.errors[:price]).to include("は9999999以下の値にしてください")
+      @item.price = "19999999"
+      @item.valid?
+      expect(@item.errors[:price]).to include("は9999999以下の値にしてください")
     end
   end
 end
