a624b78bc1b46da17b2cc0cca7b1a69e9bd76e4d
backss3 <67251822+backss3@users.noreply.github.com>
2020-08-06 13:11:28 +0900

    new

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :cost
   belongs_to_active_hash :day
 
-  validates :image, presence: true
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do |assoc|
+    assoc.validates :image
+    assoc.validates :name
+    assoc.validates :description
+    assoc.validates :category_id
+    assoc.validates :status_id
+    assoc.validates :cost_id
+    assoc.validates :prefecture_id
+    assoc.validates :day_id
+    assoc.validates :price
+  end
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
