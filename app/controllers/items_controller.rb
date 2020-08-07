e0c591be2f0668f9358d9da04437b6e0f52fb317
backss3 <67251822+backss3@users.noreply.github.com>
2020-08-07 16:40:10 +0900

    move index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :destroy, :update]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all
@@ -50,5 +51,8 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
